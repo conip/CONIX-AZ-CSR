661b818df3ffa1d2572cee3c58a40420147d27c0
conip <51626146+conip@users.noreply.github.com>
2022-09-19 14:51:13 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,4 +6,9 @@ variable "tenant_id" {}
 
 variable "csr_admin_username" {}
 variable "csr_admin_password" {}
-variable "csr1_hostname" {}
+variable "csr_hostname" {}
+variable "csr_bgp_adv_prefixes" {}
+variable "csr_bgp_local_asn" {}
+variable "csr_loopback_ip" {}
+variable "csr_bgp_peer_list" {}
+variable "csr_ipsec_peer_list" {}
