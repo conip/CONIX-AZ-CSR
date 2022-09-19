c1f7f3e15c59cbe0f807b1da002383e00712635f
conip <51626146+conip@users.noreply.github.com>
2022-09-19 16:31:02 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,12 +1,3 @@
-output "csr-1-public-ip" {
-  description = "Public IP of created CSR"
-  value       = azurerm_public_ip.PIP-1-CSR-1.ip_address
-}
-
-output "VNG_primary_public_ip" {
-  value = data.terraform_remote_state.ars.outputs.VNG_primary_public_ip
-}
-
 output "inside_subnets" {
   value = flatten(azurerm_subnet.csr-1-sub-inside.address_prefixes)
 }
