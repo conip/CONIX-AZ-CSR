dcdd34aaf75846f05ac227b80d06144476904cf2
conip <51626146+conip@users.noreply.github.com>
2022-09-19 14:59:38 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,4 @@
 #------------ AZURE
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
 
 variable "csr_admin_username" {}
 variable "csr_admin_password" {}
