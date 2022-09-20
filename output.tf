80b5b3dc5dddf92e2b57c4c0cd1226f14c7606b2
conip <51626146+conip@users.noreply.github.com>
2022-09-20 13:51:00 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
-output "inside_subnets" {
-  value = flatten(azurerm_subnet.csr-1-sub-inside.address_prefixes)
+output "csr_private_ip" {
+  value = azurerm_network_interface.csr-1-nic-1.private_ip_addresses
 }
 output "csr_public_ip" {
   value = azurerm_public_ip.PIP-1-CSR-1.ip_address
